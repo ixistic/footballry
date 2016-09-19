72647ba27db9ab77c8ad75f59f58bfb754b09269
ixistic <teeravipark@gmail.com>
2016-09-20 01:18:30 +0700

    Fixed Rcov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,6 @@
 
 ENV['RAILS_ENV'] ||= 'test'
 require 'simplecov'
-require 'simplecov-rcov'
 SimpleCov.start 'rails'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
