1524c5fc5d2ae79408a8e5d37ec7191c67a85bf9
ixistic <teeravipark@gmail.com>
2016-10-10 23:39:30 +0700

    Add production key

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,8 @@ set :default_env, {
     'http_proxy' => '192.41.170.23:3128',
     'https_proxy' => '192.41.170.23:3128',
     'ftp_proxy' => '192.41.170.23:3128',
-    'HTTPS_PROXY_REQUEST_FULLURI' => 'false'
+    'HTTPS_PROXY_REQUEST_FULLURI' => 'false',
+    'SECRET_KEY_BASE' => '9480572e38abce1ea276c3766abb3910631077569e1f22c54913fbfd2c51643cf4c951893035adf6e855c431c1fc2a7eb0e35cdbcf1687e7710473366aff252b'
 }
 
 set :deploy_to, '/home/deploy/web8/footballry'
