6c2fc7ec2b80c6ae830eaa0b6cbe6bd45774a74d
ixistic <teeravipark@gmail.com>
2016-10-12 00:11:08 +0700

    finish add role

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -9,7 +9,11 @@ RailsAdmin.config do |config|
   # config.current_user_method(&:current_user)
 
   ## == Cancan ==
-  # config.authorize_with :cancan
+  config.authorize_with :cancan
+
+  config.authorize_with do
+    redirect_to main_app.root_path unless current_user.admin?
+  end
 
   ## == Pundit ==
   # config.authorize_with :pundit
