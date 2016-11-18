8d9bde688ccc62422f9dd645f1e649bf1d6573cc
ixistic <teeravipark@gmail.com>
2016-11-18 17:27:57 +0700

    fix header

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,9 @@ module FootballryV001
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.web_console.development_only = false
+
+    config.action_dispatch.default_headers = {
+        'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
