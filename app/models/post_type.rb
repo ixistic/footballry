b865af5d277a147000f8ec001390219958bac8a6
ixistic <teeravipark@gmail.com>
2016-12-04 17:54:52 +0700

    Fix bug

--- app/models/post_type.rb
+++ app/models/post_type.rb
@@ -1,7 +1,6 @@
 class PostType < ApplicationRecord
-  attr_accessor :created_by, :last_edited_by
-  belongs_to :created_by, class_name: "User", foreign_key: "created_by_id"
-  belongs_to :last_edited_by, class_name: "User", foreign_key: "last_edited_by_id"
+  belongs_to :created_by, class_name: "User"
+  belongs_to :last_edited_by, class_name: "User"
   has_many :posts, dependent: :destroy
 
   has_paper_trail
