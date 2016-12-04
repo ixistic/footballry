abdd81a84b995ff95d0873af832ac402d8dc7311
ixistic <teeravipark@gmail.com>
2016-12-04 16:15:17 +0700

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,13 @@
 @admin = Role.create(name: "Admin")
 @regular = Role.create(name: "Regular")
 # Admin
-User.create(email: "admin@ait.asia", password: "qwertyui",
+@user_admin = User.create(email: "admin@ait.asia", password: "qwertyui",
             password_confirmation: "qwertyui", role_id: @admin.id)
 # Regular
-User.create(email: "regular@ait.asia", password: "qwertyui",
+@user_regular = User.create(email: "regular@ait.asia", password: "qwertyui",
             password_confirmation: "qwertyui", role_id: @regular.id)
+
+PostType.create(name: "News articles", created_by_id: @user_admin.id,last_edited_by_id: @user_admin.id)
+PostType.create(name: "Video clips", created_by_id: @user_admin.id,last_edited_by_id: @user_admin.id)
+PostType.create(name: "Forum posts", created_by_id: @user_admin.id,last_edited_by_id: @user_admin.id)
+PostType.create(name: "Product posts", created_by_id: @user_admin.id,last_edited_by_id: @user_admin.id)
