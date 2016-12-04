6da92b00444f0631544b142ffab7c75235e99a90
ixistic <teeravipark@gmail.com>
2016-12-04 15:11:32 +0700

    Add oauth for facebook

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   resources :post_types
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
   get 'home/index'
 
   root 'home#index'
