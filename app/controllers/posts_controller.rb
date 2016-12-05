d0f6ee37ba93c01a61515f92d1954296aaaef23b
ixistic <teeravipark@gmail.com>
2016-12-05 16:37:14 +0700

    Add gitignore for upload image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -82,6 +82,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body, :topic_id, :post_type_id)
+      params.require(:post).permit(:title, :body, :topic_id, :post_type_id, :avatar)
     end
 end
