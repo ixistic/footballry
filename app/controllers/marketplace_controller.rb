626d3382498939070f9f7c541ccbfc59f5ef0ada
ixistic <teeravipark@gmail.com>
2016-12-06 05:01:16 +0700

    Add search feature

--- app/controllers/marketplace_controller.rb
+++ app/controllers/marketplace_controller.rb
@@ -2,7 +2,12 @@ class MarketplaceController < ApplicationController
   def index
     @topic = Topic.find_by(:title => 'Marketplace')
     if @topic.present?
-      @market = Post.where(:topic_id => @topic.id).order(updated_at: :desc)
+      @market = Post.where(:topic_id => @topic.id)
+    end
+    if params[:q].present?
+      @market = @market.where("lower(body) LIKE lower('%#{params[:q]}%')").order(updated_at: :desc)
+    else
+      @market = @market.order(updated_at: :desc)
     end
   end
 end
--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -2,7 +2,12 @@ class VideoController < ApplicationController
   def index
     @topic = Topic.find_by(:title => 'Video')
     if @topic.present?
-      @video = Post.where(:topic_id => @topic.id).order(updated_at: :desc)
+      @video = Post.where(:topic_id => @topic.id)
+    end
+    if params[:q].present?
+      @video = @video.where("lower(title) LIKE lower('%#{params[:q]}%')").order(updated_at: :desc)
+    else
+      @video = @video.order(updated_at: :desc)
     end
   end
 end
